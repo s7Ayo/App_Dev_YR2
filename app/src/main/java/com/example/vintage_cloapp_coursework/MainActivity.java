f7f80121fb669f8a3722954ddc69a9eb241e8b74
s7Ayo <96587946+s7Ayo@users.noreply.github.com>
2022-12-16 19:35:47 +0000

    Nav Pages semi done

--- app/src/main/java/com/example/vintage_cloapp_coursework/MainActivity.java
+++ app/src/main/java/com/example/vintage_cloapp_coursework/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.vintage_cloapp_coursework;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
-import android.widget.Toolbar;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_basket, R.id.nav_Favorite,R.id.nav_settings)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
---
+++ app/src/main/java/com/example/vintage_cloapp_coursework/ui/gallery/Settings.java
@@ -0,0 +1,24 @@
+package com.example.vintage_cloapp_coursework.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.vintage_cloapp_coursework.R;
+
+public class Settings extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_settings,container, false);
+
+        return root;
+
+    }
+
+}
