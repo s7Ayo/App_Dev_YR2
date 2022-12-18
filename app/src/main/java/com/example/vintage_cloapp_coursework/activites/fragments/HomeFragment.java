87c843647523e8decd3ace510e730b1acc279a96
s7Ayo <96587946+s7Ayo@users.noreply.github.com>
2022-12-18 22:41:12 +0000

    rotator scrollbar finished card view started

--- app/src/main/java/com/example/vintage_cloapp_coursework/activites/fragments/HomeFragment.java
+++ app/src/main/java/com/example/vintage_cloapp_coursework/activites/fragments/HomeFragment.java
@@ -9,19 +9,49 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.vintage_cloapp_coursework.R;
+import com.example.vintage_cloapp_coursework.adapters.HomeHorAdapter;
 import com.example.vintage_cloapp_coursework.databinding.FragmentHomeBinding;
+import com.example.vintage_cloapp_coursework.models.HomeHorModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+        RecyclerView homeHorizontalRec;
+        List<HomeHorModel> homeHorModelList;
+        HomeHorAdapter homeHorAdapter;
+
 
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         View root = inflater.inflate(R.layout.fragment_home,container, false);
 
+        homeHorizontalRec = root.findViewById(R.id.home_hor_rec);
+        homeHorizontalRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
 
+
+        homeHorModelList = new ArrayList<>();
+
+        homeHorModelList.add(new  HomeHorModel(R.drawable.jacket_icon,"Jacket"));
+        homeHorModelList.add(new  HomeHorModel(R.drawable.tshirt_icon,"Tshirt"));
+        homeHorModelList.add(new  HomeHorModel(R.drawable.shorts_icon,"Shorts"));
+        homeHorModelList.add(new  HomeHorModel(R.drawable.pants_icons,"Pants"));
+        homeHorModelList.add(new  HomeHorModel(R.drawable.hats_icon,"Hats"));
+        homeHorModelList.add(new  HomeHorModel(R.drawable.shoes_icon,"Shoes"));
+        homeHorModelList.add(new  HomeHorModel(R.drawable.bags_icons,"Bags"));
+
+        homeHorAdapter = new HomeHorAdapter(getActivity(),homeHorModelList);
+        homeHorizontalRec.setAdapter(homeHorAdapter);
+        homeHorizontalRec.setHasFixedSize(true);
+        homeHorizontalRec.setNestedScrollingEnabled(false);
         return root;
     }
 
--- app/src/main/java/com/example/vintage_cloapp_coursework/adapters/HomeHorAdapter.java
+++ app/src/main/java/com/example/vintage_cloapp_coursework/adapters/HomeHorAdapter.java
@@ -1,26 +1,63 @@
 package com.example.vintage_cloapp_coursework.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.vintage_cloapp_coursework.R;
+import com.example.vintage_cloapp_coursework.models.HomeHorModel;
+
+import java.util.List;
+
 public class HomeHorAdapter extends RecyclerView.Adapter<HomeHorAdapter.ViewHolder>{
 
+    Context context;
+    List<HomeHorModel> list;
+
+    public HomeHorAdapter(Context context, List<HomeHorModel> list) {
+        this.context = context;
+        this.list = list;
+    }
 
     @NonNull
     @Override
-    public HomeHorAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_horizontal_item,parent,false));
+
+
+
+
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HomeHorAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(list.get(position).getImage());
+        holder.name.setText(list.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.hor_img);
+            name =  itemView.findViewById(R.id.hor_text);
+
+
+        }
     }
 }
