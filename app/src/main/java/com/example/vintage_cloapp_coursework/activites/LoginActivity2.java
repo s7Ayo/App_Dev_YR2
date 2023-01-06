c677a80516fc62de6164c304099273652cfa30fd
s7Ayo <96587946+s7Ayo@users.noreply.github.com>
2023-01-06 00:54:49 +0000

    login on theory works now work on cart

--- app/src/main/java/com/example/vintage_cloapp_coursework/activites/LoginActivity2.java
+++ app/src/main/java/com/example/vintage_cloapp_coursework/activites/LoginActivity2.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vintage_cloapp_coursework.MainActivity;
 import com.example.vintage_cloapp_coursework.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.security.MessageDigest;
 import java.util.Base64;
@@ -18,19 +25,18 @@ public class LoginActivity2 extends AppCompatActivity {
     private EditText emailinput;
     private EditText passwordinput;
     Button button;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-        emailinput = findViewById(R.id.email_address_input_loginpage);
-        passwordinput = findViewById(R.id.password_input_loginpage);
         button.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
-                String email = emailinput.getText().toString();
+                final String email = emailinput.getText().toString();
                 String password  = passwordinput.getText().toString();
                 try {
                     MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
@@ -40,10 +46,42 @@ public class LoginActivity2 extends AppCompatActivity {
                     return;
                 }
 
-            }
-        } );
+                // Retrieve data from the database
+                //DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+                String finalPassword = password;
+                root.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        // Check if email exists in the database
+                        if (dataSnapshot.child("users").hasChild(email)) {
+                            // Email exists, check if password is correct
+                            String correctPassword = dataSnapshot.child("users").child(email).child("password").getValue(String.class);
+                            if (finalPassword.equals(correctPassword)) {
+                                // Password is correct, allow user to sign in
+                                Toast.makeText(getApplicationContext(), "LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity2.this, MainActivity.class));
+                            } else {
+                                // Password is incorrect, show error message
+                                Toast.makeText(getApplicationContext(), "LOGIN UNSUCCESSFULL ", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            // Email does not exist, show error message
+                            Toast.makeText(getApplicationContext(), "LOGIN UNSUCCESSFULL ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        return;
+                    }
+
+
+                });
+            }
+        });
     }
+
+
 
     public void register(View view) {
         startActivity(new Intent(LoginActivity2.this, RegistrationActivity2.class));
